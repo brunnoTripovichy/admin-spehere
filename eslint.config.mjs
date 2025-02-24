fa89b96b8f682139004a1366c1cda184a363b6c2
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-24 18:47:54 -0500

    chore: add extra libs and configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import prettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...pluginQuery.configs["flat/recommended"],
+  {
+    plugins: {
+      prettier,
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
 
 export default eslintConfig;
