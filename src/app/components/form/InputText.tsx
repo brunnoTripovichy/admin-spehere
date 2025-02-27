40e2a481658d3ca80e97bc26e6c2696d1921a9e1
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-27 09:47:52 -0500

    feat: adjust error state

--- src/app/components/form/InputText.tsx
+++ src/app/components/form/InputText.tsx
@@ -27,11 +27,10 @@ const InputText = forwardRef<HTMLInputElement, InputTextProps>(
           id={id}
           ref={ref}
           className={`w-full px-3 py-2 sm:px-4 sm:py-2.5 md:px-5 md:py-3 
-                      text-xs sm:text-sm md:text-base border rounded-md bg-white dark:bg-gray-900
-                      text-gray-800 dark:text-gray-100 shadow-sm transition-all duration-200
-                      focus:ring-2 focus:ring-blue-400 focus:outline-none 
-                      border-gray-300 dark:border-gray-700 hover:border-gray-400
-                      ${error ? 'border-red-500 focus:ring-red-400' : ''}`}
+            text-xs sm:text-sm md:text-base border rounded-md bg-white dark:bg-gray-900
+            text-gray-800 dark:text-gray-100 shadow-sm transition-all duration-200
+            focus:ring-2 focus:outline-none 
+            ${error ? 'border-red-500 focus:ring-red-400 hover:border-red-400' : 'border-gray-300 dark:border-gray-700 focus:ring-blue-400 hover:border-gray-400'}`}
           {...props}
         />
 
