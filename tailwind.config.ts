0c66a5892a306f24293de05462c638378cc90173
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-27 10:59:23 -0500

    feat: theme selector

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'selector',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        primaryHover: 'var(--primary-hover)',
+        secondary: 'var(--secondary)',
+        secondaryHover: 'var(--secondary-hover)',
+        borderColor: 'var(--border)',
+        cardBg: 'var(--card-bg)',
       },
     },
   },
